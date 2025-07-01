50d109b504d903575d3dee3622a94ebf61c5175a
Maryam Khaled Abdallah <maryamkhaledabobakr@gmail.com>
2025-07-01 19:28:23 +0300

    update the server.js file and remove the web.config file

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
